3fe5ee91bf2da0e2aaee52918a90a35cab59d68c
Tibet Tornaci <oofdere@hey.com>
2025-02-17 23:55:52 -0800

    update config

--- config.nix
+++ config.nix
@@ -1,70 +1,80 @@
-{ self, pkgs, ... }: {
-      imports = [
-        ./apps/fonts.nix
-      ];
+{
+  self,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./apps/fonts.nix
+  ];
 
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.nano
-          pkgs.tailscale
-          pkgs.nushell
-          pkgs.rustup
-          pkgs.bun
-          pkgs.elixir
-        ];
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [
+    pkgs.nano
+    pkgs.tailscale
+    pkgs.nushell
+    pkgs.rustup
+    pkgs.bun
+    pkgs.elixir
+  ];
 
-      homebrew = {
-        enable = true;
-        onActivation = {
-          autoUpdate = true;
-          cleanup = "uninstall";
-          upgrade = true;
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "uninstall";
+      upgrade = true;
+    };
 
-          
-        };
+    taps = [];
+    brews = ["mpv"];
+    casks = [
+      "floorp"
+      "alt-tab"
+      "stats"
+      "pearcleaner"
+      "qlmarkdown"
+      "github"
+    ];
+  };
 
-        taps = [ ];
-          brews = [ "mpv" ];
-          casks = [ 
-            "floorp"
+  nixpkgs.config.allowUnfree = true;
 
-          ];
-      };
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
 
-      nixpkgs.config.allowUnfree = true;
+  services.tailscale.enable = true;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-      
-      services.tailscale.enable = true;
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;
+  users.users.teo = {
+    name = "teo";
+    home = "/Users/teo";
+    shell = pkgs.nushell;
+  };
 
-      users.users.teo = {
-        name = "teo";
-        home = "/Users/teo";
-        shell = pkgs.nushell;
-      };
+  # make finder suck less balls
+  system.defaults.finder = {
+    _FXShowPosixPathInTitle = true; # show full path in finder title
+    AppleShowAllExtensions = true; # show all file extensions
+    FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
+    QuitMenuItem = true; # enable quit menu item
+    ShowPathbar = true; # show path bar
+    ShowStatusBar = true; # show status bar
+    CreateDesktop = false; # hide desktop icons
+  };
 
-      # make finder suck less balls
-      system.defaults.finder = {
-        _FXShowPosixPathInTitle = true;  # show full path in finder title
-        AppleShowAllExtensions = true;  # show all file extensions
-        FXEnableExtensionChangeWarning = false;  # disable warning when changing file extension
-        QuitMenuItem = true;  # enable quit menu item
-        ShowPathbar = true;  # show path bar
-        ShowStatusBar = true;  # show status bar
-      };
+  # enable touchid for sudo
+  security.pam.enableSudoTouchIdAuth = true;
 
-      # Set Git commit hash for darwin-version.
-      #system.configurationRevision = self.rev or self.dirtyRev or null;
+  # Set Git commit hash for darwin-version.
+  #system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    }
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
--- home.nix
+++ home.nix
@@ -1,23 +1,29 @@
-{ config, pkgs, ... }: {
-    home.username = "teo";
-    home.homeDirectory = "/Users/teo";
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.username = "teo";
+  home.homeDirectory = "/Users/teo";
 
-    home.stateVersion = "24.11";
-    
-imports = [
+  home.stateVersion = "24.11";
+
+  imports = [
     ./apps/git.nix
     ./apps/bash.nix
     ./apps/codium.nix
   ];
 
-    home.packages = [
-        pkgs.htop
-        pkgs.neofetch
-        pkgs.zed-editor
-    ];
+  home.packages = [
+    pkgs.htop
+    pkgs.neofetch
+    pkgs.zed-editor
+    pkgs.raycast
+    (pkgs.tree-sitter.override {webUISupport = true;})
+  ];
 
-    programs.nushell.enable = true;
+  home.shell.enableShellIntegration = true;
+  programs.nushell.enable = true;
 
-
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 }
